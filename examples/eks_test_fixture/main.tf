c040641d9b934c673b0b68d6ffb45e2546e92d13
drauba <drauba@forsythe.com>
2019-02-08 10:10:53 -0600

    Update main.tf

--- examples/eks_test_fixture/main.tf
+++ examples/eks_test_fixture/main.tf
@@ -86,6 +86,7 @@ locals {
     Environment = "test"
     GithubRepo  = "terraform-aws-eks"
     GithubOrg   = "terraform-aws-modules"
+    consul-cluster = "${var.consul_cluster_tag_key}" = "${var.consul_cluster_name}"
   }
 }
 
@@ -172,7 +173,4 @@ module "eks" {
   map_users_count                      = "${var.map_users_count}"
   map_accounts                         = "${var.map_accounts}"
   map_accounts_count                   = "${var.map_accounts_count}"
-  tags = {
-        "${var.consul_cluster_tag_key}" = "${var.consul_cluster_name}"
-    }
 }
